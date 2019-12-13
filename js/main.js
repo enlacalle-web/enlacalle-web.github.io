7e42e8075ad45a3c6245b44423a0757e6c725032
enlacalle-web <58847760+enlacalle-web@users.noreply.github.com>
2019-12-13 09:35:38 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,286 @@
+//Get Modal Element
+var modal = document.getElementsByClassName('modal');
+//Get open modal button
+
+//Primeira fileira
+var btn = document.getElementsByClassName('Appbutton');
+
+
+
+
+// Get CloseBtn
+var closeBtn = document.getElementsByClassName('closeBtn');
+
+
+//Listen for open click
+
+//Planear
+btn[0].onclick = function(){
+  modal[0].style.display = "block";
+}
+
+closeBtn[0].onclick = function() {
+  modal[0].style.display = "none";
+}
+
+
+//Marplast
+btn[1].onclick = function(){
+  modal[1].style.display = "block";
+
+}
+closeBtn[1].onclick = function() {
+  modal[1].style.display = "none";
+  
+}
+
+//Marplast
+btn[2].onclick = function(){
+  modal[2].style.display = "block";
+
+}
+closeBtn[2].onclick = function() {
+  modal[2].style.display = "none";
+  
+}
+
+//Bifarma
+btn[3].onclick = function(){
+  modal[3].style.display = "block";
+
+}
+closeBtn[3].onclick = function() {
+  modal[3].style.display = "none";
+  
+}
+
+//Monteiro
+btn[4].onclick = function(){
+  modal[4].style.display = "block";
+
+}
+closeBtn[4].onclick = function() {
+  modal[4].style.display = "none";
+  
+}
+
+//StarShop
+btn[5].onclick = function(){
+  modal[5].style.display = "block";
+
+}
+closeBtn[5].onclick = function() {
+  modal[5].style.display = "none";
+  
+}
+
+//Tiago
+btn[6].onclick = function(){
+  modal[6].style.display = "block";
+
+}
+closeBtn[6].onclick = function() {
+  modal[6].style.display = "none";
+  
+}
+
+//FF Sound
+btn[7].onclick = function(){
+  modal[7].style.display = "block";
+
+}
+closeBtn[7].onclick = function() {
+  modal[7].style.display = "none";
+  
+}
+
+//Irmãos Marques
+btn[8].onclick = function(){
+  modal[8].style.display = "block";
+
+}
+closeBtn[8].onclick = function() {
+  modal[8].style.display = "none";
+  
+}
+
+//O novo
+btn[9].onclick = function(){
+  modal[9].style.display = "block";
+
+}
+closeBtn[9].onclick = function() {
+  modal[9].style.display = "none";
+  
+}
+
+//Clube On
+btn[10].onclick = function(){
+  modal[10].style.display = "block";
+
+}
+closeBtn[10].onclick = function() {
+  modal[10].style.display = "none";
+  
+}
+
+//Rcm
+btn[11].onclick = function(){
+  modal[11].style.display = "block";
+
+}
+closeBtn[11].onclick = function() {
+  modal[11].style.display = "none";
+  
+}
+
+
+//Iem
+btn[12].onclick = function(){
+  modal[12].style.display = "block";
+
+}
+closeBtn[12].onclick = function() {
+  modal[12].style.display = "none";
+  
+}
+
+//Dr Diego
+btn[13].onclick = function(){
+  modal[13].style.display = "block";
+
+}
+closeBtn[13].onclick = function() {
+  modal[13].style.display = "none";
+  
+}
+
+//Concept Internet
+btn[14].onclick = function(){
+  modal[14].style.display = "block";
+
+}
+closeBtn[14].onclick = function() {
+  modal[14].style.display = "none";
+  
+}
+
+//Pastel da Praça Gourmet
+btn[15].onclick = function(){
+  modal[15].style.display = "block";
+
+}
+closeBtn[15].onclick = function() {
+  modal[15].style.display = "none";
+  
+}
+
+//PrimusCart
+btn[16].onclick = function(){
+  modal[16].style.display = "block";
+
+}
+closeBtn[16].onclick = function() {
+  modal[16].style.display = "none";
+  
+}
+
+//Aqui Tem
+btn[17].onclick = function(){
+  modal[17].style.display = "block";
+
+}
+closeBtn[17].onclick = function() {
+  modal[17].style.display = "none";
+  
+}
+
+//IDC Telecom
+btn[18].onclick = function(){
+  modal[18].style.display = "block";
+
+}
+closeBtn[18].onclick = function() {
+  modal[18].style.display = "none";
+  
+}
+
+//Ziluka Sorvetes
+btn[19].onclick = function(){
+  modal[19].style.display = "block";
+
+}
+closeBtn[19].onclick = function() {
+  modal[19].style.display = "none";
+  
+}
+
+//Pingado
+btn[20].onclick = function(){
+  modal[20].style.display = "block";
+
+}
+closeBtn[20].onclick = function() {
+  modal[20].style.display = "none";
+  
+}
+
+//Prefeitura de Guararema
+btn[21].onclick = function(){
+  modal[21].style.display = "block";
+
+}
+closeBtn[21].onclick = function() {
+  modal[21].style.display = "none";
+  
+}
+
+//Rosa Maria Imóveis
+btn[22].onclick = function(){
+  modal[22].style.display = "block";
+
+}
+closeBtn[22].onclick = function() {
+  modal[22].style.display = "none";
+  
+}
+
+//Priscila Cavinatti
+btn[23].onclick = function(){
+  modal[23].style.display = "block";
+
+}
+closeBtn[23].onclick = function() {
+  modal[23].style.display = "none";
+  
+}
+
+//Bruvine
+btn[24].onclick = function(){
+  modal[24].style.display = "block";
+
+}
+closeBtn[24].onclick = function() {
+  modal[24].style.display = "none";
+  
+}
+
+//Rotary Club Guararema
+btn[25].onclick = function(){
+  modal[25].style.display = "block";
+
+}
+closeBtn[25].onclick = function() {
+  modal[25].style.display = "none";
+  
+}
+
+//Bem Amigos Grill
+btn[26].onclick = function(){
+  modal[26].style.display = "block";
+
+}
+closeBtn[26].onclick = function() {
+  modal[26].style.display = "none";
+  
+}
